b89333642896810b8a2183539426a2f9cb7d69fd
Jai Ganesh <73826061+Jg-07@users.noreply.github.com>
2021-03-16 11:24:56 +0530

    Update PIG.js

--- PIG.js
+++ PIG.js
@@ -7,5 +7,5 @@ x.addEventListener("click", function () {
     var i = Math.floor(Math.random() * Math.floor(6)) + 1;
     console.log(i);
     var j = i.toString();
-    y.src = "http://127.0.0.1:5500/Frame%20" + j + ".png";
+    y.src =  "Frame"+j+".png";
 });
